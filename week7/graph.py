4acb671ec127a5501912aafee30a123248869d70
Balthild <ibalthild@gmail.com>
2019-04-17 03:31:07 +0800

    Week 7: Add labels to bar chart

--- week7/graph.py
+++ week7/graph.py
@@ -1,9 +1,22 @@
 import os
 
-import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 from pandas import DataFrame, read_csv, Series
+
+
+def autolabel(rects):
+    """
+    https://matplotlib.org/examples/api/barchart_demo.html
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width() / 2., height + 0.1,
+                 '%d' % int(height),
+                 ha='center', va='bottom')
+
 
 mpl.rcParams['font.family'] = 'FandolHei'
 
@@ -39,9 +52,13 @@ for cl in ['三5', '三8', '三9']:
     split_at = avg.searchsorted(range(10, 91, 10))
     intervals: np.ndarray = np.split(avg, split_at)
 
-    plt.bar([f'{x}+' for x in range(0, 91, 10)], [y.size for y in intervals])
+    r = plt.bar([f'{x}+' for x in range(0, 91, 10)], [y.size for y in intervals])
+    autolabel(r)
+
     plt.title(cl)
     plt.ylabel('Distributions')
+
+    plt.tight_layout()
     plt.savefig(f'graphs/{cl}_dist.png', dpi=300)
     plt.close()
 
